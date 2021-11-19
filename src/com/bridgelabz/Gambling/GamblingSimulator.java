515ee8a1ef58a3a004e4adb2e3a3ed3dd3702f89
pknowledge <abhishekrawane1195@gmail.com>
2021-11-19 23:08:36 +0530

    Modified : continue playing next month or stop gampling logic

--- src/com/bridgelabz/Gambling/GamblingSimulator.java
+++ src/com/bridgelabz/Gambling/GamblingSimulator.java
@@ -1,78 +1,70 @@
 package com.bridgelabz.Gambling;
 
-import java.util.Scanner;
-
 public class GamblingSimulator {
 
-	private static final int STAKE = 100;
-	private static final int BET = 1;
+	public static final int STAKE = 100;
+	public static final int BET = 1;
 	private static final int IS_WIN = 1;
 	private static final int IS_LOOSE = 0;
 	private static final int NO_OF_DAYS_IN_MONTH = 30;
 
-	static int maxWinArray[] = new int[30];
-	static int maxLostArray[] = new int[30];
 	static int totalStackValue = 100;
 	static int dailyBet = 0;
 
-	public void maxWinOrMAxLostInDay() {
-		int maxWinAmount=0,maxlostAmount=0,maxWinDay=0,maxLostDay=0,winDay=0,lostDay=0;
-		for (int i = 0; i < 30; i++) {
-			
-			if(maxWinArray[i]>maxWinAmount) {
-				maxWinAmount=maxWinArray[i];
-				winDay=i;
-			}
-			
-			if(maxLostArray[i] > maxlostAmount) {
-				maxlostAmount = maxLostArray[i];
-				lostDay=i;
-			}
-		}
-		System.out.println("my luckiest day where i am won maximum in a day " + winDay +  " is : " + maxWinAmount);
-		System.out.println("my unluckiest day where i am lost maximum in a day " +lostDay + " is : " + maxlostAmount);	
-	}
-	
-	public void dailyBet() {
+	public static int dailyBet() {
 		while (dailyBet <= STAKE / 2) {
 
 			double randomCheck = Math.random() * 2;
 			switch ((int) randomCheck) {
-				case IS_WIN:
-					totalStackValue += BET;
-					break;
-				default:
-					totalStackValue -= BET;
+			case IS_WIN:
+				totalStackValue += BET;
+				break;
+			case IS_LOOSE:
+				totalStackValue -= BET;
 			}
 			dailyBet += BET;
 		}
 		dailyBet = 0;
-		
+		return totalStackValue;
 	}
 
-	public static void main(String[] args) {
-		int noOfDays = 0;
-		GamblingSimulator gamblingSimulator = new GamblingSimulator();
+	public void monthlyGamblingBet() {
 
+		int maxWinAmount = 0, maxLostAmount = 0;
+		int winDay = 0, lostDay = 0;
 
-		System.out.println("gambler starting with a stake of : $" + STAKE + " and bet in every game is : $" + BET);
+		int maxWinArray[] = new int[30];
+		int maxLostArray[] = new int[30];
 
+		int noOfDays = 0, winCount = 0, lostCount = 0;
 		while (noOfDays < NO_OF_DAYS_IN_MONTH) {
-
-			gamblingSimulator.dailyBet(); 
+			totalStackValue = 100;
+			dailyBet();
 			if (totalStackValue > STAKE) {
-				System.out.println("win amount in a day " + noOfDays + " is  " + (totalStackValue - STAKE));
-				maxWinArray[noOfDays] = (totalStackValue - STAKE);
-			}
 
-			else if (totalStackValue < STAKE) {
-				System.out.println("lost amount in a day " + noOfDays + " is : " + (STAKE - totalStackValue));
-				maxLostArray[noOfDays] = (STAKE - totalStackValue);
+				System.out.println("win amount in a day " + (noOfDays + 1) + " is  " + (totalStackValue - STAKE));
+				maxWinArray[winCount] = (totalStackValue - STAKE);
+				if (maxWinArray[winCount] > maxWinAmount) {
+					maxWinAmount = maxWinArray[winCount];
+					winDay = (noOfDays + 1);
+				}
+				winCount++;
+			} else if (totalStackValue < STAKE) {
+
+				System.out.println("lost amount in a day " + (noOfDays + 1) + " is : " + (STAKE - totalStackValue));
+				maxLostArray[lostCount] = (STAKE - totalStackValue);
+				if (maxLostArray[lostCount] > maxLostAmount) {
+					maxLostAmount = maxLostArray[lostCount];
+					lostDay = (noOfDays + 1);
+				}
+				lostCount++;
 			} else {
-				System.out.println("no lost no win ");
+				
+				System.out.println(" In day " + (noOfDays + 1) + " no lost no win ");
 			}
 			noOfDays++;
 		}
-		gamblingSimulator.maxWinOrMAxLostInDay();
+		System.out.println("my luckiest day where i am won maximum in a day " + winDay + " is : " + maxWinAmount);
+		System.out.println("my unluckiest day where i am lost maximum in a day " + lostDay + " is : " + maxLostAmount);
 	}
 }
